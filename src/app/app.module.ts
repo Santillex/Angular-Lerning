2216949ea2fac11e7f1cfc289874d999cd357254
Santiago S <sseguel@meducacion.neuquen.gob.ar>
2022-01-21 14:29:55 -0300

    Se subiron cosas con bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PersonasComponent } from './personas/personas.component';
+import { PersonaComponent } from './persona/persona.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, PersonasComponent, PersonaComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/personas/personas.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-personas',
+    templateUrl: './personas.component.html',
+    //styleUrls: ['./personas.component.css'] 
+    styles: [`
+        h1{
+            color: blue;
+        }
+    `]
+})
+export class PersonasComponent{
+
+}
