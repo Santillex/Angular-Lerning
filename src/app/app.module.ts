7162b77fe3cd9f834ab97d7592c104d900503402
Santiago <santiago20.19@outlook.com>
2022-01-31 15:05:11 -0300

    cambios en componente personas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
 import { PersonaComponent } from './persona/persona.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent, PersonasComponent, PersonaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -9,7 +9,13 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar = false;
     mensaje = 'No se ha agregado ninguna persona';
+    titulo = '';
+
     agregarPersona(){
         this.mensaje = 'Persona agregada';
     }
+
+    /*modificarTitulo(event: Event){
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }*/
 }
